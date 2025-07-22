d714575293edfc2274888d462edbecbc6a2ef450
Romyr <romyrlopez123@gmail.com>
2025-07-23 01:55:47 +0800

    completed reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    split = string.split("");
+    string_len = string.length;
+    let reverse = "";
+    for(let i = string_len; i>0 ; i--){
+    reverse += split[i-1];
+}
+    return reverse
 };
+
+console.log(reverseString(''))
 
 // Do not edit below this line
 module.exports = reverseString;

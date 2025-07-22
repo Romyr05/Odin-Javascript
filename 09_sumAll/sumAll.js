3e1984589d139a8e827691cac9a7d9482fd89696
Romyr <romyrlopez123@gmail.com>
2025-07-23 02:30:11 +0800

    done SUM sumAll
    used a Number.isInteger to check if its really an isInteger
    output of the function is true or false only

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    result = 0
+    if(start < 0 || !Number.isInteger(start) || !Number.isInteger(end) ){
+        return "ERROR"
+    }
+    bigger = (start > end ) ? start : end
+    smaller = (start < end ) ? start : end
 
+    while(smaller <= bigger)
+    {
+        result = result+smaller
+        smaller++;
+    }
+    return result
 };
+
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;

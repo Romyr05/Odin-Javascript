9a37b7fe1e82bf91ef1dfbe9b76f5049522eb1ee
Romyr <romyrlopez123@gmail.com>
2025-07-23 02:16:25 +0800

    Finished removeFromArray Comment below
    MAKE SURE THAT IF YOU SPLICED AN ARRAY THAT YOU ADJUST THE SIZE OF YOURE LOOP
    SINCE WE ARE SUBTRACTING ONE ELEMENT SO THE SIZE WILL DIFFER FROM THE ORIGINAL

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...number) {
+    for(let i =0; i<array.length;i++)
+    {
+        for(let j = 0; j<number.length;j++){
+            console.log(number[j])
+            if(number[j] === array[i])
+            {
+                array.splice(i,1)
+                i--
+            }
+        }
+    }
+    return array
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

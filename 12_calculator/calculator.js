4023147a4c52be06da72e3eb60b565224cd34878
Romyr <romyrlopez123@gmail.com>
2025-07-23 23:45:16 +0800

    done calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2
+};
+
+const sum = function(numbers) {
+	arr_len = numbers.length
+  all_sum = 0
+  for(let i =0 ;i<arr_len; i++){
+    all_sum = all_sum + numbers[i]
+    console.log(numbers[i])
+  }
+  console.log(`rawr ${arr_len}`)
+  return all_sum;
+};
+
+const multiply = function(numbers) {
+	arr_len = numbers.length
+  all_multi = 1;
+  for(let i =0 ;i<arr_len; i++){
+    all_multi = all_multi * numbers[i]
+    console.log(numbers[i])
+  }
+  return all_multi;
+};
+
+const power = function(num1, pow) {
+	return num1 ** pow
+};
+
+const factorial = function(number) {
+  if (number <= 0 || number === 1)
+  {
+    return 1;
+  }
+  else{
+    return number * factorial(number-1)
+  }
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(multiply([2, 4, 6, 8, 10, 12, 14]))
 
 // Do not edit below this line
 module.exports = {

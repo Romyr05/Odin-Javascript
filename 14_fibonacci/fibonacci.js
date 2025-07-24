0125a006d10415224d6b8bdd9d126b83fa182fa9
Romyr <romyrlopez123@gmail.com>
2025-07-25 01:39:52 +0800

    Made a non recursive fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n<0){
+        return "OOPS"
+    }
+    if(n <=1){
+        return n
+    }
+    current = 0
+    next = 1;
+
+    for(let i = 2; i<=n; i++){
+        result = current + next;
+        current = next;
+        next = result;
+    }
+    return result;
 
 };
+
+console.log(fibonacci("-21"));
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;

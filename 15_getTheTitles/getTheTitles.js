c88ce0ad21c5d083be95bde0276295a0cc17a7c2
Romyr <romyrlopez123@gmail.com>
2025-07-25 01:52:08 +0800

    Finished getting the titles exercise, used ForEach
    map can also be used to iterate each array

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+
+//maps the array and then for each array it will pass the argument 
+// inside the arrow function which it will use to pass the function
+const getTheTitles = function(array) {
+    const bookie = []
+    array.forEach(book => {
+        bookie.push(book.title)
+    });
+    return bookie
 };
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;

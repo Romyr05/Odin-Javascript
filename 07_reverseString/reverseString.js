87ab526df27cb185c7e0e732d88dec7ca3b6ec50
Romyr <romyrlopez123@gmail.com>
2025-08-15 09:49:35 +0800

    made a reverseString without using a method

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,14 +1,18 @@
-const reverseString = function(string) {
-    split = string.split("");
-    string_len = string.length;
-    let reverse = "";
-    for(let i = string_len; i>0 ; i--){
-    reverse += split[i-1];
-}
-    return reverse
-};
+function reverseString_nomethod(str){
+    let reversed = ""
+    split_str = str.split(" ")
+    console.log(split_str.length)
+    for(let i = 0; i < split_str.length;i++){
+        for(let j = split_str[i].length -1; j>= 0; j--)
+        {
+            reversed = reversed + split_str[i][j]
+        }
+        reversed += " "
+    }
+    console.log(reversed)
+} 
 
-console.log(reverseString(''))
+reverseString('hello there')
 
 // Do not edit below this line
 module.exports = reverseString;

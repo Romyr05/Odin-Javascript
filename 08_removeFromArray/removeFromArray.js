7d528f24c64977af505f1167f44e8edc363e2023
Romyr <romyrlopez123@gmail.com>
2025-08-15 10:12:22 +0800

    own solution method to removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,17 +1,16 @@
-const removeFromArray = function(array,...number) {
-    for(let i =0; i<array.length;i++)
-    {
-        for(let j = 0; j<number.length;j++){
-            console.log(number[j])
-            if(number[j] === array[i])
-            {
+const removeFromArray_ownSOL = function(array,...number) {
+    for(let i = 0; i< array.length; i++){
+        for(let j = 0; j < number.length; j++){
+            if(array[i] === number[j]){
                 array.splice(i,1)
                 i--
             }
         }
+
     }
     return array
 };
+
 
 console.log(removeFromArray([1, 2, 3, 4], 3,2));
 
